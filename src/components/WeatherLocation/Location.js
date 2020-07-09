99aaffd98658b7523d1834913fa1fb69825b3fa3
raul soissa <raulsoissa@gmail.com>
2020-07-08 23:52:10 -0400

    first commit

---
+++ src/components/WeatherLocation/Location.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Location = (props) => {
+    // Destructuring
+    const {city} = props;
+    return (
+        <div><h1>{city}</h1></div>
+    );
+};
+
+Location.propTypes = {
+    city: PropTypes.string.isRequired,
+}
+
+export default Location;
---
+++ src/components/WeatherLocation/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Location from './Location';
+import WeatherData from './WeatherData';
+
+const WeatherLocation = () => (
+    <div>
+        <Location city={"Santiago"}></Location>
+        <WeatherData></WeatherData>
+    </div>    
+);
+
+
+export default WeatherLocation;
